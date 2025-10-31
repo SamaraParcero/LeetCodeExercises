f6f3df8cbc0842434e05abee88ed0c321b7dfc4b
SamaraParcero <samaraparceroti@gmail.com>
2025-10-31 18:30:04 -0300

    Add LeetCode Challenge 7

---
+++ LeetCodeChallenges/LeetCodeChallenge7/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace LeetCodeChallenge7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Program.Reverse(123));
+            Console.WriteLine(Program.Reverse(123));
+            Console.WriteLine(Program.Reverse(120));
+
+        }
+
+        public static int Reverse(int x)
+        {
+            int reversed = 0;
+
+            while (x != 0)
+            {
+
+                int num = x % 10;
+
+                x = x / 10;
+
+                reversed = reversed * 10 + num;
+
+                if (reversed < -2147483648 || reversed > 2147483647)
+                {
+                    return 0;
+                }
+
+            }
+
+            return reversed;
+
+        }
+    }
+}
